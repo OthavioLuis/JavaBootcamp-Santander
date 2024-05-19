fd2e4e326f9cef046fb96ed8d3969ac0d0aced37
OthavioLuis <othaviorodriguesf@gmail.com>
2024-05-19 20:22:53 -0300

    Anatomia de Casses - 3

--- anatomia-classes/Anatomia das Classes commits/src/MinhaClasse.java
+++ anatomia-classes/Anatomia das Classes commits/src/MinhaClasse.java
@@ -1,5 +1,24 @@
 public class MinhaClasse {
     public static void main (String [] args) {
-        //variaveis aceitas são as que tem $ ou _ e letra maíuscula
+        /*String meuNome = "Othavio";
+
+        int anoFabricacao = 2022;
+
+        boolean verdadeira = true; 
+
+        anoFabricacao = 2018; //Pode ser alterado*/
+        
+        //pode atribuir um valor ou não nas variaveis
+
+        String primeiroNome = "Othavio";
+        String segundoNome = "Rodrigues";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
